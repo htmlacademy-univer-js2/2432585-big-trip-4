16e8208298fb9d83cd806d254c905f89200d8d47
Darya Palitsyna <dasha.palitsyna.04@gmail.com>
2024-05-07 15:40:10 +0500

    Создание презентера для точки

---
+++ src/presenter/point-presenter.js
@@ -0,0 +1,76 @@
+import { remove, render, replace } from "../framework/render";
+import ListPointsView from "../view/list-points-view";
+import EditPointView from "../view/editing-form-view";
+
+export default class PointPresenter {
+  #pointListContainer = null;
+
+  #pointComponent = null;
+  #pointEditComponent = null;
+
+  #destinationsModel = null;
+  #offersModel = null;
+
+  #point = null;
+
+  constructor({pointListContainer, destinationsModel, offersModel}) {
+    this.#pointListContainer = pointListContainer;
+    this.#destinationsModel = destinationsModel;
+    this.#offersModel = offersModel;
+  }
+
+  init(point) {
+    this.#point = point;
+
+    const prevPointComponent = this.#pointComponent;
+    const prevPointEditComponent = this.#pointEditComponent;
+
+    this.#pointComponent = new ListPointsView({
+      data: this.#point,
+      destinations: this.#destinationsModel.getDestinationById(point.destinations),
+      offers: this.#offersModel.getOffersByType(point.type),
+      onEditClick: pointEditClickHandler
+    })
+
+    this.#pointEditComponent = new EditPointView({
+      point: this.#point,
+      pointDestinations: this.#destinationsModel.get(),
+      pointOffers: this.#offersModel.get(),
+      onResetClick: resetButtonClickHandler,
+      onSubmitClick: pointSubmitHandler
+    })
+
+    function replaceEditToPoint() {
+      replace(prevPointComponent, prevPointEditComponent);
+    }
+
+    function replacePointToEdit() {
+      replace(prevPointEditComponent, prevPointComponent);
+    }
+
+    const escKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceEditToPoint();
+        document.removeEventListener('keydown', escKeyDown);
+      }
+    };
+
+    function pointEditClickHandler() {
+      replacePointToEdit();
+      document.addEventListener('keydown', escKeyDown);
+    }
+
+    function resetButtonClickHandler() {
+      replaceEditToPoint();
+      document.removeEventListener('keydown', escKeyDown);
+    }
+
+    function pointSubmitHandler() {
+      replaceEditToPoint();
+      document.removeEventListener('keydown', escKeyDown);
+    }
+
+    /* render(pointComponent, this.#pointListContainer.element); */
+  }
+}
--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,8 +1,7 @@
-import EditPointView from '../view/editing-form-view.js';
-import ListPointsView from '../view/list-points-view.js';
 import ListView from '../view/list-view.js';
 import EventListEmptyView from '../view/event-list-empty-view.js';
-import {render, replace} from '../framework/render.js';
+import {render, remove} from '../framework/render.js';
+import PointPresenter from './point-presenter.js';
 
 export default class TripPresenter {
   #listComponent = new ListView();
@@ -24,63 +23,36 @@ export default class TripPresenter {
   }
 
   init() {
+    this.#renderBoard();
+  }
+
+  #renderPoint(point) {
+    const pointPresenter = new PointPresenter({
+      pointListContainer: this.#listComponent.element,
+    });
+
+    pointPresenter.init(point);
+  };
+
+  #renderPoints = () => {
+    this.#tripPoint.forEach((point) => {
+      this.#renderPoint(point);
+    });
+  };
+
+  #renderPointContainer = () => {
+    this.currentPoint = this.#pointsModel.points;
+
+    render(this.#listComponent,this.#listContainer);
+  }
+
+  #renderBoard = () => {
     if (this.#tripPoint.length === 0) {
       render(new EventListEmptyView(), this.#listContainer);
       return;
     }
 
-    this.currentPoint = this.#pointsModel.points;
-
-    render(this.#listComponent,this.#listContainer);
-
-    this.#tripPoint.forEach((point) => {
-      this.#renderPoint(point);
-    });
-  }
-
-  #renderPoint(point) {
-    const pointComponent = new ListPointsView ({
-      data: point,
-      onEditClick: pointEditClickHandler
-    });
-
-    const editingForm = new EditPointView({
-      point,
-      onSubmitClick: pointSubmitHandler,
-      onResetClick: resetButtonClickHandler
-    });
-
-    function replaceEditToPoint() {
-      replace(pointComponent, editingForm);
-    }
-
-    function replacePointToEdit() {
-      replace(editingForm, pointComponent);
-    }
-
-    const escKeyDown = (evt) => {
-      if (evt.key === 'Escape' || evt.key === 'Esc') {
-        evt.preventDefault();
-        replaceEditToPoint();
-        document.removeEventListener('keydown', escKeyDown);
-      }
-    };
-
-    function pointEditClickHandler() {
-      replacePointToEdit();
-      document.addEventListener('keydown', escKeyDown);
-    }
-
-    function resetButtonClickHandler() {
-      replaceEditToPoint();
-      document.removeEventListener('keydown', escKeyDown);
-    }
-
-    function pointSubmitHandler() {
-      replaceEditToPoint();
-      document.removeEventListener('keydown', escKeyDown);
-    }
-
-    render(pointComponent, this.#listComponent.element);
-  }
+    this.#renderPointContainer();
+    this.#renderPoints();
+  };
 }
